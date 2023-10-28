bd253ce24232fc7755ae70505c72974c99b84dbc
Joel Chi <abongsjoel@gmail.com>
2023-10-28 10:15:07 +0100

    Test rendering of table content

--- src/components/UserList/UserList.test.js
+++ src/components/UserList/UserList.test.js
@@ -14,3 +14,34 @@ test("render one row per user", () => {
 
   expect(rows).toHaveLength(2);
 });
+
+test("render one row per user 2", () => {
+  const users = [
+    { name: "Mascot", email: "mas@mas.com" },
+    { name: "Faith", email: "faith@mas.com" },
+  ];
+
+  const { container } = render(<UserList users={users} />);
+
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll("tbody tr");
+
+  expect(rows).toHaveLength(2);
+});
+
+test("render the email and name of each user", () => {
+  const users = [
+    { name: "Mascot", email: "mas@mas.com" },
+    { name: "Faith", email: "faith@mas.com" },
+  ];
+
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
